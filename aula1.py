cac9cc5c41ea36c807bc35a586bc16dfb516a011
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2023-05-26 17:09:22 -0300

    ok

---
+++ aula1.py
@@ -0,0 +1,10 @@
+# comentários  em linhas
+'''
+docstring pode ser usada para comentar várias linhas
+o interpretador do python lê(mas ignora) tudo que estiver dentro de 3 aspas simples ou duplas
+'''
+"""
+aspas duplas também podem ser usadas
+"""
+print("Hello World!")
+print("1, 2, 3, 4, 5, 6, 7, 8, 9, 10")
