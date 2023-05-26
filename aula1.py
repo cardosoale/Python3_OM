b3d08b05d28996a65e5ba57707298ee2acdab3a7
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2023-05-26 18:04:45 -0300

    teste

--- aula1.py
+++ aula1.py
@@ -5,6 +5,7 @@ o interpretador do python lê(mas ignora) tudo que estiver dentro de 3 aspas sim
 '''
 """
 aspas duplas também podem ser usadas
+teste github
 """
 print("Hello World!")
 print("1, 2, 3, 4, 5, 6, 7, 8, 9, 10")
