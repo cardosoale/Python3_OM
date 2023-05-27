beba7b95bcbfb5175144e8b0d998b71cb5cd6f9a
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2023-05-27 12:18:18 -0300

    ok

---
+++ aula2.py
@@ -0,0 +1,6 @@
+# \r\n ->CRLF(Windows)
+# \n ->LF(Linux e MacOS)
+
+print(12, 34, 1011, sep="-", end="#\n")
+print(56, 78, sep="*")
+print(9, 10, sep="---")
---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Alexandre'
+sobrenome = 'Cardoso'
+idade = 46
+ano_de_nascimento = 2023 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.75
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_de_nascimento)
+print('Maior de idade: ', maior_de_idade)
+print('Altura em metros: ', altura_metros)    
