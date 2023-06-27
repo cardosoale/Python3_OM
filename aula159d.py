845e5f6331f02e2e5dcbf164a9c3fe4c23275fb2
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2023-06-27 12:47:21 -0300

    att

---
+++ aula159d.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+
+@dataclass(repr=True, order=False)
+class Pessoa:
+    nome: str
+    sobrenome: str
+
+
+if __name__ == '__main__':
+    lista = [Pessoa('A', 'Z'), Pessoa('B', 'Y'), Pessoa('C', 'X')]
+    ordenadas = sorted(lista, reverse=True, key=lambda p: p.sobrenome)
+    print(ordenadas)
