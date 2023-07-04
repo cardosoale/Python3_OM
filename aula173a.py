c6dbeed977944f8447dff6040d5f3bb89063d9dc
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2023-07-04 19:26:39 -0300

    att

--- aula173a.py
+++ aula173a.py
@@ -2,12 +2,21 @@ import os
 import shutil
 
 HOME = os.path.expanduser('~')
-DESKTOP = os.path.join(HOME, 'Área de Trabalho')
+DESKTOP = os.path.join(HOME, 'Desktop')
 PASTA_ORIGINAL = os.path.join(DESKTOP, 'Faculdade')
 NOVA_PASTA = os.path.join(DESKTOP, 'NOVA_PASTA')
 
 os.makedirs(NOVA_PASTA, exist_ok=True)
 
 for root, dirs, files in os.walk(PASTA_ORIGINAL):
+    for dir_ in dirs:
+        caminho_novo_diretorio = os.path.join(
+            root.replace(PASTA_ORIGINAL, NOVA_PASTA), dir_
+        )
+        os.makedirs(caminho_novo_diretorio, exist_ok=True)
     for file in files:
-        print(file)
+        caminho_arquivo = os.path.join(root, file)
+        caminho_novo_arquivo = os.path.join(
+            root.replace(PASTA_ORIGINAL, NOVA_PASTA), file
+        )
+        shutil.copy(caminho_arquivo, caminho_novo_arquivo)
