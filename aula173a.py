e6fe9ba583c6ef97163511b79fa1c8237f5c0462
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2023-07-04 14:34:58 -0300

    att

---
+++ aula173a.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+HOME = os.path.expanduser('~')
+DESKTOP = os.path.join(HOME, 'Área de Trabalho')
+PASTA_ORIGINAL = os.path.join(DESKTOP, 'Faculdade')
+NOVA_PASTA = os.path.join(DESKTOP, 'NOVA_PASTA')
+
+os.makedirs(NOVA_PASTA, exist_ok=True)
+
+for root, dirs, files in os.walk(PASTA_ORIGINAL):
+    for file in files:
+        print(file)
